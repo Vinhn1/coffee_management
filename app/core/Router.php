7a7a4fc0efac445dc0aa077006646c1f47dfb7a2
Vinhn1 <vinhnguyen04275@gmail.com>
2025-09-27 21:24:06 +0700

    Khởi tạo router và gọi router trong index

---
+++ app/core/Router.php
@@ -0,0 +1,32 @@
+<?php
+    class Router{
+        public function run(){
+            $controller = $_GET['controller'] ?? 'home';
+            $action = $_GET['action'] ?? 'index';
+
+            // Danh sách controller và action 
+            $controllerList = array(
+                'home' => ['index', 'error'],
+            );
+
+            // Kiểm tra nếu không có controller tồn tại thì trả về index/error
+            if(!array_key_exists($controller, $controllerList) || !in_array($action, $controllerList[$controller])){
+                $controller = 'home';
+                $action = 'error';
+            }
+
+        // Nạp file php của controller tương ứng (home_controller.php)
+        include_once __DIR__ . "/../controllers/" . $controller . "_controller.php";
+
+        // Khởi tạo đối tượng controller và gọi action (HomeController)
+            $className = ucfirst($controller) . "Controller"; 
+            $controllerObj = new $className();
+
+            if(isset($_GET['id'])){
+                $controllerObj->$action($_GET['id']);
+            }else{
+                $controllerObj->$action();
+            }
+        }
+    }
+?>
--- config/db.php
+++ config/db.php
@@ -1,37 +1,38 @@
 <?php
-    require_once('./constants.php');
-    class Database{
-        private static $instance = null;
-        private $conn;
+require_once('constants.php');
+class Database
+{
+    private static $instance = null;
+    private $conn;
 
-        // Để private không cho tạo đối tượng new Database() trực tiếp bên ngoài
-        private function __construct()
-        {
-            try{
-                $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
-                $this->conn = new PDO($dsn, DB_USER, DB_PASS);
+    // Để private không cho tạo đối tượng new Database() trực tiếp bên ngoài
+    private function __construct()
+    {
+        try {
+            $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
+            $this->conn = new PDO($dsn, DB_USER, DB_PASS);
 
-                // Thiet lap che do bao loi
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                // Tắt giả lập tăng tính an toàn khi chạy câu lệnh sql
-                $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-            }catch(PDOException $e){
-                die("Lỗi kết nối: " . $e->getMessage());
-            }
-        }
-
-        // Singleton: Lấy thể hiện duy nhất
-        public static function getInstance()
-        {
-            if (self::$instance == null) {
-                self::$instance = new Database();
-            }
-            return self::$instance;
-        }
-
-        // Lấy kết nối PDO
-        public function getConnection(){
-            return $this->conn;
+            // Thiet lap che do bao loi
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // Tắt giả lập tăng tính an toàn khi chạy câu lệnh sql
+            $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        } catch (PDOException $e) {
+            die("Lỗi kết nối: " . $e->getMessage());
         }
     }
-?>
+
+    // Singleton: Lấy thể hiện duy nhất
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    // Lấy kết nối PDO
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+}
